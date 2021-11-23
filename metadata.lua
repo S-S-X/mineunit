28fffc9504fee914c3bbfcf05636a7a01aa91a5d
SX <50966843+S-S-X@users.noreply.github.com>
2021-11-23 20:34:18 +0200

    Update NodeMetaRef, MetaDataRef and InvRef

--- metadata.lua
+++ metadata.lua
@@ -1,103 +1,19 @@
 
 mineunit("common/misc_helpers")
 
---
--- InvList
---
-
 local function assert_invlist_index(index, size)
 	assert(type(index) == "number" and math.floor(index) == index, "InvList:set_stack: Invalid InvList stack index")
 	assert(index > 0, "InvList:set_stack: InvList stack index should never be less than 1")
 	assert(index <= size, "InvList:set_stack: Invalid InvList stack index larger than InvList size")
 end
 
-local InvList = {}
-InvList.__index = InvList
-
-function InvList:_set_size(size)
-	assert(size >= 0, "InvList:set_size: Negative size not acceptable")
-	if size < #self then
-		for i = #self, size + 1, -1 do
-			table.remove(self, i)
-		end
-	end
-	for i = 1, size do
-		if not self[i] then
-			self[i] = ItemStack()
-		end
-	end
-end
-
-function InvList:_get_count()
-	-- FIXME: should go through items and execute is_empty for each, possibly remove completely
-	return self._count
-end
-
-function InvList:_is_empty()
-	-- FIXME: should go through items and execute is_empty for each, possibly remove completely
-	return self:_get_count() < 1
-end
-
-function InvList:_set_stack(index, itemstack)
-	-- FIXME: tracking item count is inaccurate if data is modified directly, remove method and only allow direct access
-	assert_invlist_index(index, #self)
-	local stack = ItemStack(itemstack)
-	local self_empty = self[index]:is_empty()
-	local stack_empty = stack:is_empty()
-	if self_empty and not stack_empty then
-		-- Increment counter if adding new stack that is not empty
-		self._count = self._count + 1
-	elseif stack_empty and not self_empty then
-		-- Decrement counter if replacing existing stack with empty stack
-		self._count = self._count - 1
-	end
-	self[index] = stack
-end
-
-function InvList:_add_stack(itemstack)
-	if not ItemStack(itemstack):is_empty() then
-		for index = 1, #self do
-			if self[index]:is_empty() then
-				-- Empty slot found
-				self:_set_stack(index, itemstack)
-				return true
-			elseif self[index]:item_fits(itemstack) then
-				-- Slot with enough space found
-				self[index]:add_item(itemstack)
-				return true
-			end
-		end
-	end
-end
-
-function InvList:__tostring()
+local function invlist_tostring(list)
 	local items = {}
-	for _, item in ipairs(self) do
+	for _, item in ipairs(list) do
 		table.insert(items, tostring(item))
 	end
-	return ('InvList({%s}, %d, %d)'):format(table.concat(items, ","), self:_get_count(), #self)
+	return ('InvList({%s}, %d)'):format(table.concat(items, ","), #list)
 end
-
-mineunit.export_object(InvList, {
-	private = true,
-	name = "InvList",
-	typename = "table",
-	constructor = function(self, value)
-		local obj = {
-			_count = 0,
-		}
-		setmetatable(obj, InvList)
-		if type(value) == "table" then
-			obj:_set_size(#value)
-			for _, stack in ipairs(value) do
-				obj:_add_stack(stack)
-			end
-		elseif value ~= nil then
-			error("TYPE NOT IMPLEMENTED: " .. type(value))
-		end
-		return obj
-	end
-})
 
 --
 -- InvRef
@@ -106,23 +22,53 @@ mineunit.export_object(InvList, {
 local InvRef = {}
 -- * `is_empty(listname)`: return `true` if list is empty
 function InvRef:is_empty(listname)
-	return (not self._lists[listname]) or self._lists[listname]:_is_empty()
+	assert.is_string(listname, "InvRef:is_empty listname must be string. Got "..type(listname))
+	if self._empty[listname] ~= nil then
+		return self._empty[listname]
+	elseif self._lists[listname] then
+		for _, stack in ipairs(self._lists[listname]) do
+			if not stack:is_empty() then
+				self._empty[listname] = false
+				return false
+			end
+		end
+		self._empty[listname] = true
+	end
+	return true
 end
 -- * `get_size(listname)`: get size of a list
 function InvRef:get_size(listname)
-	return self._lists[listname] and #self._lists[listname] or 0
+	return self._sizes[listname] or 0
 end
 -- * `set_size(listname, size)`: set size of a list
 --    * returns `false` on error (e.g. invalid `listname` or `size`)
 function InvRef:set_size(listname, size)
-	if size and size == tonumber(size) then
-		if not self._lists[listname] then
-			self._lists[listname] = InvList()
+	local newsize = tonumber(size)
+	if newsize < 0 then
+		return false
+	elseif newsize == 0 then
+		self._lists[listname] = nil
+		self._empty[listname] = nil
+		newsize = nil
+	elseif not self._lists[listname] then
+		self._lists[listname] = {}
+		setmetatable(self._lists[listname], { __tostring = invlist_tostring })
+		self._empty[listname] = true
+		for index = 1, newsize do
+			self._lists[listname][index] = ItemStack()
 		end
-		self._lists[listname]:_set_size(size)
-		return true
+	elseif newsize > self._sizes[listname] then
+		for index = self._sizes[listname], newsize do
+			self._lists[listname][index] = ItemStack()
+		end
+	elseif newsize < self._sizes[listname] then
+		self._empty[listname] = self._empty[listname] and true or nil
+		for index = self._sizes[listname], newsize, -1 do
+			self._lists[listname][index] = nil
+		end
 	end
-	return false
+	self._sizes[listname] = newsize
+	return true
 end
 -- * `get_width(listname)`: get width of a list
 function InvRef:get_width(listname)
@@ -136,17 +82,29 @@ end
 function InvRef:get_stack(listname, index)
 	local list = self:get_list(listname)
 	assert(list, "InvRef:set_stack: Invalid inventory list " .. tostring(list))
-	assert_invlist_index(index, #list)
-	return ItemStack(list[index])
+	assert_invlist_index(index, self:get_size(listname))
+	return list[index] and ItemStack(list[index]) or ItemStack()
 end
 -- * `set_stack(listname, i, stack)`: copy `stack` to index `i` in list
-function InvRef:set_stack(listname, i, stack)
-	local list = self:get_list(listname)
+function InvRef:set_stack(listname, index, stack)
+	local list = self._lists[listname]
 	assert(list, "InvRef:set_stack: Invalid inventory list " .. tostring(list))
-	list:_set_stack(i, stack)
+	assert_invlist_index(index, self:get_size(listname))
+	-- Either clone or create itemstack, both cases are needed here. References should not be added to lists.
+	stack = ItemStack(stack)
+	local input_empty = stack:is_empty()
+	if not self._empty[listname] or not list[index]:is_empty() then
+		if input_empty then
+			self._empty[listname] = self._sizes[listname] == 1 and true or nil
+		end
+	elseif not input_empty then
+		self._empty[listname] = false
+	end
+	list[index] = stack
 end
 -- * `get_list(listname)`: return full list
 function InvRef:get_list(listname)
+	assert(self._lists[listname], "InvRef:get_list list not found: "..tostring(listname))
 	mineunit:debug("InvRef:get_list returning list "..listname.." as reference, this can lead to unxpected results")
 	return self._lists[listname]
 end
@@ -154,7 +112,12 @@ end
 function InvRef:set_list(listname, list)
 	assert.is_string(listname, "InvRef:set_list expected `listname` to be string, got "..type(listname))
 	assert.is_table(list, "InvRef:set_list expected `list` to be table, got " .. type(list))
-	self._lists[listname] = InvList(list)
+	local target = self._lists[listname]
+	assert(target, "InvRef:set_list list does not exist "..tostring(listname))
+	self._empty[listname] = nil
+	for index = 1, math.min(self._sizes[listname], #list) do
+		target[index] = ItemStack(list[index])
+	end
 end
 -- * `get_lists()`: returns list of inventory lists
 function InvRef:get_lists()
@@ -166,13 +129,31 @@ function InvRef:get_lists()
 end
 -- * `set_lists(lists)`: sets inventory lists (size will not change)
 function InvRef:set_lists(lists)
-	error("NOT IMPLEMENTED")
+	for listname,list in pairs(lists) do
+		self:set_list(listname, list)
+	end
 end
 -- * `add_item(listname, stack)`: add item somewhere in list, returns leftover
 function InvRef:add_item(listname, stack)
-	local list = self:get_list(listname)
-	assert(list, "InvRef:set_stack: Invalid inventory list " .. tostring(list))
-	list:_add_stack(stack)
+	local list = self._lists[listname]
+	assert(list, "InvRef:add_item: Invalid inventory list " .. tostring(list))
+	stack = mineunit.utils.type(stack) == "ItemStack" and stack or ItemStack(stack)
+	local count = stack:get_count()
+	if not stack:is_empty() then
+		for index = 1, self:get_size(listname) do
+			-- Try to add items into stack, get and check leftover stack
+			stack = list[index]:add_item(stack)
+			if stack:is_empty() then
+				-- Input stack is empty, all items have been added somewhere
+				self._empty[listname] = false
+				return stack
+			end
+		end
+		if stack:get_count() < count then
+			self._empty[listname] = false
+		end
+	end
+	return stack
 end
 -- * `room_for_item(listname, stack)`: returns `true` if the stack of items can be fully added to the list
 function InvRef:room_for_item(listname, stack)
@@ -199,17 +180,17 @@ function InvRef:contains_item(listname, stack, match_meta)
 				return true
 			end
 			local meta2 = liststack:get_meta():to_table()
-			if mineunit.utils.count(meta1) == mineunit.utils.count(meta2) then
-				local matching = true
-				for k,v in pairs(meta1) do
-					if not v == meta2[k] then
-						matching = false
-						break
-					end
+			local fieldcount = 0
+			local matching = true
+			for k,v in pairs(meta1) do
+				if not v == meta2[k] then
+					matching = false
+					break
 				end
-				if matching then
-					return true
-				end
+				fieldcount = fieldcount + 1
+			end
+			if matching then
+				return mineunit.utils.count(meta2) == fieldcount
 			end
 		end
 	end
@@ -235,13 +216,16 @@ mineunit.export_object(InvRef, {
 	constructor = function(self, value)
 		local obj
 		if value == nil then
-			obj = {}
+			obj = {
+				_lists = {},
+				_sizes = {},
+				_empty = {},
+			}
 		elseif mineunit.utils.type(value) == "InvRef" then
 			obj = table.copy(value)
 		else
 			error("TYPE NOT IMPLEMENTED: " .. type(value))
 		end
-		obj._lists = obj._lists or {}
 		setmetatable(obj, InvRef)
 		return obj
 	end,
@@ -255,8 +239,10 @@ local MetaDataRef = {}
 function MetaDataRef:contains(key) return self._data[key] ~= nil end
 function MetaDataRef:get(key) return self._data[key] end
 function MetaDataRef:set_string(key, value)
-	value = value ~= nil and tostring(value) or ""
-	self._data[key] = value ~= "" and value or nil
+	value = (value ~= nil and value ~= "") and tostring(value) or nil
+	if self._data[key] ~= value then
+		self._data[key] = value
+	end
 end
 function MetaDataRef:get_string(key) return self._data[key] or "" end
 function MetaDataRef:set_int(key, value) self:set_string(key, math.floor(value)) end
@@ -270,7 +256,13 @@ function MetaDataRef:to_table()
 	end
 	return { fields = fields }
 end
-function MetaDataRef:from_table(t) error("NOT IMPLEMENTED") end
+function MetaDataRef:from_table(t)
+	assert(type(t) == "table", "MetaDataRef:from_table expects table as argument")
+	if t.fields then
+		assert(type(t.fields) == "table", "MetaDataRef:from_table expects table fields to be table")
+		self._data = table.copy(t.fields)
+	end
+end
 function MetaDataRef:equals(other) error("NOT IMPLEMENTED") end
 
 mineunit.export_object(MetaDataRef, {
@@ -301,9 +293,24 @@ function NodeMetaRef:mark_as_private(...) mineunit:info("NodeMetaRef:mark_as_pri
 
 function NodeMetaRef:to_table()
 	local result = MetaDataRef.to_table(self)
-	-- TODO: result.inventory might not be in correct format, verify and fix if needed
 	result.inventory = self:get_inventory():get_lists()
 	return result
+end
+
+function NodeMetaRef:from_table(t)
+	assert(type(t) == "table", "NodeMetaRef:from_table expects table as argument")
+	if t.fields then
+		MetaDataRef.from_table(self, t)
+	end
+	if t.inventory then
+		assert(type(t.inventory) == "table", "NodeMetaRef:from_table expects table inventory to be table")
+		local inv = self:get_inventory()
+		for listname, list in pairs(t.inventory) do
+			inv:set_size(listname, #list)
+			inv:set_list(listname, list)
+		end
+	end
+	self._data = table.copy(t.fields)
 end
 
 mineunit.export_object(NodeMetaRef, {
@@ -319,7 +326,7 @@ mineunit.export_object(NodeMetaRef, {
 			error("TYPE NOT IMPLEMENTED: " .. type(value))
 		end
 		obj._data = obj._data or {}
-		obj._inventory = InvRef()
+		obj._inventory = obj._inventory or InvRef()
 		setmetatable(obj, NodeMetaRef)
 		return obj
 	end,
