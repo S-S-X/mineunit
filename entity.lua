062617f787c93ad7fbfc2bb4556b5b4032adef67
Luke aka SwissalpS <Luke@SwissalpS.ws>
2022-02-09 22:24:14 +0100

    attach methods

--- entity.lua
+++ entity.lua
@@ -15,7 +15,12 @@ function ObjectRef:set_roll(value) self._roll = value end
 
 
 function ObjectRef:get_pos() return table.copy(self._pos) end
-function ObjectRef:set_pos(value) self._pos = vector.new(value) end
+function ObjectRef:set_pos(value)
+	self._pos = vector.copy(value)
+	for _, child in ipairs(self:get_children()) do
+		child:set_pos(vector.add(self._pos, child._attach.position))
+	end
+end
 function ObjectRef:get_velocity() return table.copy(self._velocity) end
 function ObjectRef:add_velocity(vel)
 	-- * `vel` is a vector, e.g. `{x=0.0, y=2.3, z=1.0}`
@@ -91,25 +96,56 @@ function ObjectRef:set_animation_frame_speed(frame_speed)
 	-- * `frame_speed`: number, default: `15.0`
 end
 function ObjectRef:set_attach(parent, bone, position, rotation, forced_visible)
-	--
-	-- * `bone`: string. Default is `""`, the root bone
-	-- * `position`: `{x=num, y=num, z=num}`, relative, default `{x=0, y=0, z=0}`
-	-- * `rotation`: `{x=num, y=num, z=num}` = Rotation on each axis, in degrees.
-	--   Default `{x=0, y=0, z=0}`
-	-- * `forced_visible`: Boolean to control whether the attached entity
-	--    should appear in first person. Default `false`.
-	-- * This command may fail silently (do nothing) when it would result
-	--   in circular attachments.
+	if not parent then return end
+	if self._attach and self._attach.parent == parent then
+		mineunit:info('Attempt to attach to parent that object is already attached to.')
+		return
+	end
+	-- detach if attached
+	self:set_detach()
+	local obj = parent
+	while true do
+		if not obj._attach then break end
+		if obj._attach.parent == self then
+			mineunit:warning('Mod bug: Attempted to attach object to an object that '
+				.. 'is directly or indirectly attached to the first object. -> '
+				.. 'circular attachment chain.')
+			return
+		end
+		obj = obj._attach.parent
+	end
+	mineunit:info(parent._children)
+	if 'table' ~= type(parent._children) then parent._children = {} end
+	mineunit:info(parent._children)
+	table.insert(parent._children, self)
+	self._attach = {
+		parent = parent,
+		bone = bone or '',
+		position = position or vector.zero(),
+		rotation = rotation or vector.zero(),
+		forced_visible = not not forced_visible,
+	}
+	self._pitch = self._attach.position.x
+	self._roll = self._attach.position.z
+	self._yaw = self._attach.position.y
+	self:set_pos(vector.add(parent:get_pos(), self._attach.position))
+	-- TODO: bones depending on object type
 end
 function ObjectRef:get_attach()
-	--: returns parent, bone, position, rotation, forced_visible,
-	-- or nil if it isn't attached.
+	return self._attach
 end
 function ObjectRef:get_children()
-	--: returns a list of ObjectRefs that are attached to the
-	-- object.
+	return self._children or {}
 end
 function ObjectRef:set_detach()
+	if not self._attach then return end
+
+	local new_children = {}
+	for _, child in ipairs(self._attach.parent._children) do
+		if child ~= self then table.insert(new_children, child) end
+	end
+	self._attach.parent._children = new_children
+	self._attach = nil
 end
 function ObjectRef:set_bone_position(bone, position, rotation)
 	-- * `bone`: string. Default is `""`, the root bone
